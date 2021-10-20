488c8c1038d152456f5c27d01fee553b472133fc
maheshcitius <Mahesh.Ravirala@citiustech.com>
2021-10-20 11:34:26 +0530

    backend changes

--- server.js
+++ server.js
@@ -8,19 +8,34 @@ server.db = router.db
 
 const rules = auth.rewriter({
     // Permission rules
-    products: 660
+    reports: 660,
+    billing:660
   
   })
 
 server.use(middlewares)
 server.use(rules)
 server.use(auth)
+
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+    req.body.isActive  = true
+  }
+  if (req.method === 'PUT') {
+    req.body.updatedAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
 server.use(router)
 
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-    res.jsonp(req.query)
-  })
+
 
 
 
