ab65a5deb1f99a710deddf6522829f60a4f0ad22
maheshcitius <Mahesh.Ravirala@citiustech.com>
2021-11-08 14:32:23 +0530

    Latest code

--- server.js
+++ server.js
@@ -1,45 +1,38 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const auth = require('json-server-auth')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const auth = require("json-server-auth");
 
-const middlewares = jsonServer.defaults()
-server.db = router.db
+const middlewares = jsonServer.defaults();
+server.db = router.db;
 
 const rules = auth.rewriter({
-    // Permission rules
-    reports: 660,
-    billing:660
-  
-  })
+  // Permission rules
+  reports: 660,
+});
 
-server.use(middlewares)
-server.use(rules)
-server.use(auth)
-
+server.use(middlewares);
+server.use(rules);
+server.use(auth);
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
+
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-    req.body.isActive  = true
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+    req.body.isActive = true;
   }
-  if (req.method === 'PUT') {
-    req.body.updatedAt = Date.now()
+  if (req.method === "PUT") {
+    req.body.updatedAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
-server.use(router)
-
-
-
-
+server.use(router);
 
 server.listen(3003, () => {
-  console.log('JSON Server is running')
-})
-
+  console.log("JSON Server is running");
+});
